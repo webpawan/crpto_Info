f6c53a68f1abddde007bd8d3b841588ceca471d7
webpawan <pawansaini66477@gmail.com>
2023-01-12 23:27:59 +0530

    1.1.1

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
